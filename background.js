5c438e33f0a278e6b33baac657d16aa26eea2707
DoberBober <pochta.q@bk.ru>
2019-06-10 16:06:26 +0300

    Notifications.

--- background.js
+++ background.js
@@ -5,3 +5,54 @@ chrome.browserAction.onClicked.addListener(function(){
 		'url': 'index.html'
 	})
 })
+
+chrome.notifications.onClicked.addListener(function(notificationId) {
+	switch(notificationId){
+		case 'INSTALL':
+			chrome.tabs.create({url: 'options.html'});
+			break;
+		case 'REMINDER':
+			chrome.tabs.create({url: 'index.html'});
+			break;
+		default:
+			break;
+	}
+});
+
+chrome.runtime.onInstalled.addListener(function(details){
+	if(details.reason == "install" || details.reason == "update"){
+		chrome.notifications.create("INSTALL", {
+			type: "basic",
+			title: "Расширение установлено",
+			message: "Нажмите на это сообщение, чтобы настроить уведомления",
+			iconUrl: "images/logo128.png",
+		}, () => {
+
+		});
+	}
+});
+
+setInterval(() => {
+	chrome.storage.sync.get({
+		notificationsEnabled: true,
+		favoriteTime: []
+	}, function(items) {
+		if(items.notificationsEnabled && items.favoriteTime){
+			items.favoriteTime.forEach((time, index) => {
+				var tempDate = new Date();
+				var tempHours = tempDate.getHours();
+				var tempMinutes = (tempDate.getMinutes() < 10 ? '0' : '') + tempDate.getMinutes();
+					if(time.split(":")[0] == tempHours && time.split(":")[1] == tempMinutes){
+						chrome.notifications.create("REMINDER", {
+							type: "basic",
+							title: "Может сделаем тренировку для глаз?",
+							message: "Ты уже работаешь довольно долго",
+							iconUrl: "images/logo128.png",
+						}, () => {
+							
+						});
+					}
+			})
+		}
+	});
+}, 60000)
--- options.js
+++ options.js
@@ -1,10 +1,90 @@
 'use strict';
 
 const DONATE_PURPOSE = document.querySelectorAll('.modal__input');
-const DONATE_PURPOSE_LABEL = document.querySelector('.donateForm__purpose')
+const DONATE_PURPOSE_LABEL = document.querySelector('.donateForm__purpose');
 
 DONATE_PURPOSE.forEach((el) => {
 	el.addEventListener('change', () => {
 		DONATE_PURPOSE_LABEL.value = el.value
 	})
 })
+
+
+// 
+
+const ADD_TIME = document.querySelector('#addTime');
+const SAVE = document.querySelector('#save');
+const TIME = document.querySelector('.time__list');
+const TIME_TEMPLATE = document.querySelector('#timeTemplate').content.querySelector('div');
+
+var deleteTimeList = document.querySelectorAll('.delete')
+
+ADD_TIME.addEventListener('click', () => {
+	var el = TIME_TEMPLATE.cloneNode(true);
+	var del = el.querySelector('.delete');
+	del.addEventListener('click', () => {
+		del.parentNode.remove()
+	})
+	TIME.appendChild(el)
+})
+
+deleteTimeList.forEach((el) => {
+	el.addEventListener('click', () => {
+		el.parentNode.remove()
+	})
+	deleteTimeList = document.querySelectorAll('.delete');
+})
+
+
+// 
+
+const TIME_FORM = document.querySelector('#time');
+
+function save_options() {
+	event.preventDefault()
+	var inputs = document.querySelectorAll('.time__item input');
+	var notifications = document.querySelector('#notifications').checked;
+	var values = [];
+	inputs.forEach((el) => {
+		values.push(el.value)
+	})
+
+	chrome.storage.sync.set({
+		'favoriteTime': values,
+		'notificationsEnabled': notifications
+	}, () => {
+		chrome.notifications.create("NOTIFICATIONS", {
+			type: "basic",
+			title: "Сохранено успешно",
+			message: "",
+			iconUrl: "images/logo128.png",
+		}, () => {
+			
+		});
+	});
+
+}
+
+function restore_options() {
+	chrome.storage.sync.get({
+		notificationsEnabled: true,
+		favoriteTime: ["12:50", "15:50", "17:50"]
+	}, function(items) {
+		items.favoriteTime.forEach((time) => {
+			var el = TIME_TEMPLATE.cloneNode(true);
+			var del = el.querySelector('input').value = time;
+			var del = el.querySelector('.delete');
+			del.addEventListener('click', () => {
+				del.parentNode.remove()
+			})
+			TIME.appendChild(el)
+		})
+
+		if(items.notificationsEnabled){
+			document.querySelector('#notifications').checked = true
+		}
+	});
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+TIME_FORM.addEventListener('submit', save_options);
